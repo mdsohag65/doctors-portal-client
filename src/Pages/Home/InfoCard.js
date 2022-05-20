96b99ebd47a1db1597fd6df10fe6c4a0b4e01f60
mdsohag65 <sohag92ahmed@gmail.com>
2022-05-20 19:42:47 +0600

    set the footer and home page done

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InfoCard = ({ img, cardTitle, details, bgClass }) => {
     return (
-        <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+        <div className={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
             <figure className='p-5'>
                 <img src={img} alt="Album" />
             </figure>
